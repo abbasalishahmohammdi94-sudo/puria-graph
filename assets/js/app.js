ea66d6449f9ab842a2ef9cf598d41840cb8d9033
abbasalishahmohammdi94-sudo <abbasali.shahmohammdi94@gmail.com>
2026-08-18 16:22:58 +0330

    puria graph come to github !!
    ver0.3
    not respansive
    not compelete

---
+++ assets/js/app.js
@@ -0,0 +1,66 @@
+const counters = document.querySelectorAll(".stat h3");
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      const counter = entry.target;
+      const target = Number(counter.dataset.target);
+
+      let current = 0;
+
+      const duration = 2000;
+      const stepTime = duration / target;
+
+      const interval = setInterval(() => {
+        current++;
+
+        counter.textContent = current + "+";
+
+        if (current >= target) {
+          clearInterval(interval);
+        }
+      }, stepTime);
+
+      observer.unobserve(counter);
+    });
+  },
+  {
+    threshold: 0.6,
+  },
+);
+
+counters.forEach((counter) => {
+  observer.observe(counter);
+});
+
+
+
+// سوایپر
+
+
+const portfolioSwiper = new Swiper(".portfolioSwiper", {
+loop: true,
+
+grabCursor: true,
+allowTouchMove: true,
+
+navigation: {
+nextEl: ".swiper-button-next",
+prevEl: ".swiper-button-prev",
+},
+
+autoplay: {
+delay: 4000,
+disableOnInteraction: false,
+},
+
+pagination: {
+el: ".swiper-pagination",
+clickable: true,
+},
+});
+
