8d2b235518e4a72930c4ff71262d1e5402b45cfe
abbasalishahmohammdi94-sudo <abbasali.shahmohammdi94@gmail.com>
2026-08-19 12:55:35 +0330

    add respansive css
    ver0.8stable

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,3 +64,9 @@ clickable: true,
 },
 });
 
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+navLinks.classList.toggle("active");
+}); 
